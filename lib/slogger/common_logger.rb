6644b620def4ca09e239625cd2e7b3db77e5be75
Leandro Silva <leandrodoze@gmail.com>
2011-04-28 20:29:17 -0300

    fixed (but WIP)

--- lib/slogger/common_logger.rb
+++ lib/slogger/common_logger.rb
@@ -1,7 +1,7 @@
 module Slogger
   #
   # An adapter to make possible to use Slogger in application which by default
-  # use standard Ruby Logger.
+  # use standard Ruby Logger. It just exposes the same API of standard Logger.
   #
   module CommonLogger
     
@@ -16,11 +16,53 @@ module Slogger
       :fatal   => :err,
       :unknow  => :alert
     }
-  end
+    
+    FACILITY = ::Slogger::FACILITY
+    
+    attr_reader :app_name, :severity, :facility
 
-  SEVERITY.each_key do |severity|
-    define_method severity do |message, &block|
-      send SEVERITY[severity], message, &block
+    #
+    # To build a Slogger::CommonLogger instance.
+    #
+    # +app_name+::  The appliaction name to be logged
+    # +severity+::  The log severity (according to standard Ruby Logger): :unknow, :fatal,
+    #                 :error, :warning, :info, or :debug. It can be changed at anytime.
+    # +facility+::  A typical syslog facility: :kernel, :user, :mail, :daemon, :auth,
+    #                 :syslog, :lpr, :news, :uucp, :cron, :authpriv, :ftp,
+    #                 :local0, :local1, :local2, :local3, :local4, :local5,
+    #                 :local6, or :local7
+    #
+    # Raises an ArgumentError if app_name, severity, or facility is nil.
+    #
+    def initialize(app_name, severity, facility)
+      raise_argument_error_to_required_parameter "app_name" unless app_name
+      raise_argument_error_to_required_parameter "severity" unless severity
+      raise_argument_error_to_required_parameter "facility" unless facility
+
+      raise_argument_error_to_invalid_parameter "severity", "SEVERITY" unless SEVERITY[severity]
+      raise_argument_error_to_invalid_parameter "facility", "FACILITY" unless FACILITY[facility]
+      
+      @app_name = app_name
+      @severity = severity
+      @severity_as_int = SEVERITY[severity]
+      @facility = facility
+      @facility_as_int = FACILITY[facility]
+      
+      @syslogger = ::Slogger::Logger.new @app_name, SEVERITY[@severity], @facility
+    end
+
+    SEVERITY.each_key do |severity|
+      define_method severity do |message, &block|
+        @syslogger.send SEVERITY[severity], message, &block
+      end
+    end
+    
+    def raise_argument_error_to_required_parameter(param)
+      raise ArgumentError, "The '#{param}' parameter is required."
+    end
+
+    def raise_argument_error_to_invalid_parameter(param, options)
+      raise ArgumentError, "The '#{param}' parameter is invalid. Inspect the #{options} constant to know the options."
     end
   end
 end
